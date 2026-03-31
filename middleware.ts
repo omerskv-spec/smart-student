882e3361ef269cf78725523fb3bab26f9be45b5a
omerskv-spec <omerskv@gmail.com>
2026-03-31 15:37:36 +0300

    feat: middleware

---
+++ middleware.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  return NextResponse.next();
+}
+
+export const config = { matcher: ['/chat/:path*', '/onboarding/:path*'] };
